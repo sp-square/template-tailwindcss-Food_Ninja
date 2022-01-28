ff027a44b0508bd388da4e5fc6e3c89d9be48f92
Sandrine Poissonnet <spoisson@gmail.com>
2022-01-27 19:50:25 -0800

    set up custom styles in the config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
 	content: ['./src/**/*.{html,js}', './public/index.html'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#ff6363',
+				secondary: {
+					100: '#e2e2d5',
+					200: '#888883',
+				},
+			},
+			fontFamily: {
+				nunito: ['Nunito Sans'],
+			},
+		},
 	},
 	plugins: [],
 };
